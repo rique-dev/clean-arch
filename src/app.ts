84a7f41671cfdf0b33bec9592bfbc1b9f07976ed
Henrique Moreira <henryqrm@gmail.com>
2021-07-23 21:21:11 -0300

    refactor: 💡 validade cpf

--- src/app.ts
+++ src/app.ts
@@ -1,118 +1,53 @@
-import express from "express";
-import compression from "compression";  // compresses requests
-import session from "express-session";
-import bodyParser from "body-parser";
-import lusca from "lusca";
-import MongoStore from "connect-mongo";
-import flash from "express-flash";
-import path from "path";
-import mongoose from "mongoose";
-import passport from "passport";
-import bluebird from "bluebird";
-import { MONGODB_URI, SESSION_SECRET } from "./util/secrets";
+export function leaveOnlyNumbers(cpf: string) {
+  return cpf.replace(/\D/g, '')
+}
 
-// Controllers (route handlers)
-import * as homeController from "./controllers/home";
-import * as userController from "./controllers/user";
-import * as apiController from "./controllers/api";
-import * as contactController from "./controllers/contact";
+export function isValidCPFLength(cpf: string) {
+  const CPFValidLength = 11
 
-// API keys and Passport configuration
-import * as passportConfig from "./config/passport";
+  return String(cpf).length === CPFValidLength
+}
 
-// Create Express server
-const app = express();
+export function allNumbersAreEqual(cpf: string) {
+  return [...cpf].every(digit => digit === cpf[0])
+}
 
-// Connect to MongoDB
-const mongoUrl = MONGODB_URI;
-mongoose.Promise = bluebird;
+export function isValidCpf(cpf: string) {
+  if (!cpf) {
+    return false
+  }
 
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true } ).then(
-    () => { /** ready to use. The `mongoose.connect()` promise resolves to undefined. */ },
-).catch(err => {
-    console.log(`MongoDB connection error. Please make sure MongoDB is running. ${err}`);
-    // process.exit();
-});
+  cpf = leaveOnlyNumbers(cpf)
 
-// Express configuration
-app.set("port", process.env.PORT || 3000);
-app.set("views", path.join(__dirname, "../views"));
-app.set("view engine", "pug");
-app.use(compression());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(session({
-    resave: true,
-    saveUninitialized: true,
-    secret: SESSION_SECRET,
-    store: new MongoStore({
-        mongoUrl,
-        mongoOptions: {
-            autoReconnect: true
-        }
-    })
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(flash());
-app.use(lusca.xframe("SAMEORIGIN"));
-app.use(lusca.xssProtection(true));
-app.use((req, res, next) => {
-    res.locals.user = req.user;
-    next();
-});
-app.use((req, res, next) => {
-    // After successful login, redirect back to the intended page
-    if (!req.user &&
-    req.path !== "/login" &&
-    req.path !== "/signup" &&
-    !req.path.match(/^\/auth/) &&
-    !req.path.match(/\./)) {
-        req.session.returnTo = req.path;
-    } else if (req.user &&
-    req.path == "/account") {
-        req.session.returnTo = req.path;
-    }
-    next();
-});
+  if (!isValidCPFLength(cpf)) {
+    return false
+  }
 
-app.use(
-    express.static(path.join(__dirname, "public"), { maxAge: 31557600000 })
-);
+  if (allNumbersAreEqual(cpf)) {
+    return false
+  }
 
-/**
- * Primary app routes.
- */
-app.get("/", homeController.index);
-app.get("/login", userController.getLogin);
-app.post("/login", userController.postLogin);
-app.get("/logout", userController.logout);
-app.get("/forgot", userController.getForgot);
-app.post("/forgot", userController.postForgot);
-app.get("/reset/:token", userController.getReset);
-app.post("/reset/:token", userController.postReset);
-app.get("/signup", userController.getSignup);
-app.post("/signup", userController.postSignup);
-app.get("/contact", contactController.getContact);
-app.post("/contact", contactController.postContact);
-app.get("/account", passportConfig.isAuthenticated, userController.getAccount);
-app.post("/account/profile", passportConfig.isAuthenticated, userController.postUpdateProfile);
-app.post("/account/password", passportConfig.isAuthenticated, userController.postUpdatePassword);
-app.post("/account/delete", passportConfig.isAuthenticated, userController.postDeleteAccount);
-app.get("/account/unlink/:provider", passportConfig.isAuthenticated, userController.getOauthUnlink);
+  let d1, d2
+  let dg1, dg2, rest
+  let digito
+  let nDigResult
+  d1 = d2 = 0
+  dg1 = dg2 = rest = 0
 
-/**
- * API examples routes.
- */
-app.get("/api", apiController.getApi);
-app.get("/api/facebook", passportConfig.isAuthenticated, passportConfig.isAuthorized, apiController.getFacebook);
+  for (let nCount = 1; nCount < cpf.length - 1; nCount++) {
+    digito = parseInt(cpf.substring(nCount - 1, nCount))
+    d1 = d1 + (11 - nCount) * digito
+    d2 = d2 + (12 - nCount) * digito
+    // }
+  }
 
-/**
- * OAuth authentication routes. (Sign in)
- */
-app.get("/auth/facebook", passport.authenticate("facebook", { scope: ["email", "public_profile"] }));
-app.get("/auth/facebook/callback", passport.authenticate("facebook", { failureRedirect: "/login" }), (req, res) => {
-    res.redirect(req.session.returnTo || "/");
-});
-
-export default app;
+  rest = d1 % 11
+  dg1 = rest < 2 ? (dg1 = 0) : 11 - rest
+  d2 += 2 * dg1
+  rest = d2 % 11
+  if (rest < 2) dg2 = 0
+  else dg2 = 11 - rest
+  let nDigVerific = cpf.substring(cpf.length - 2, cpf.length)
+  nDigResult = '' + dg1 + '' + dg2
+  return nDigVerific == nDigResult
+}
--- test/app.test.ts
+++ test/app.test.ts
@@ -1,9 +1,50 @@
-import request from "supertest";
-import app from "../src/app";
+import { isValidCpf } from '../src/app'
 
-describe("GET /random-url", () => {
-    it("should return 404", (done) => {
-        request(app).get("/reset")
-            .expect(404, done);
-    });
-});
+/**
+ *
+ * Testes
+  1 - Não deve fazer um pedido com cpf inválido
+  2 - Deve fazer um pedido com 3 itens (com descrição, preço e quantidade)
+  3 - Deve fazer um pedido com cupom de desconto (percentual sobre o total do pedido)
+ *
+ */
+
+const validCPFs = [
+  '987.654.321-00',
+  '334.504.660-11',
+  '870.376.850-38',
+  '92890487008',
+]
+const invalidCPFs = [
+  '999.999.999-99',
+  '123.456.789-10',
+  '123123123',
+  '870.376.85a-38',
+]
+describe('projeto - parte 1', () => {
+  describe('Validando CPF', () => {
+    const testValidCPF = it.each(validCPFs)
+    const testInvalidCPF = it.each(invalidCPFs)
+
+    testValidCPF('CPF %o Deve ser válido', cpf => {
+      expect(isValidCpf(cpf)).toBeTruthy()
+    })
+
+    testInvalidCPF('CPF %o Deve ser inválido', cpf => {
+      expect(isValidCpf(cpf)).toBeFalsy()
+    })
+
+    it('Não deve fazer um pedido com cpf inválido', () => {})
+
+    it.todo('Deve fazer um pedido com cpf válido')
+  })
+
+  describe('Validar pedido', () => {
+    it.todo(
+      'Deve fazer um pedido com 3 itens (com descrição, preço e quantidade)'
+    )
+    it.todo(
+      'Deve fazer um pedido com cupom de desconto (percentual sobre o total do pedido)'
+    )
+  })
+})
